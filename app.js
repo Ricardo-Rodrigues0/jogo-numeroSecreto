b2d6b50512a706556fb431c24638fb5832ed0585
Ricardo Rodrigues <ricardo272530@gmail.com>
2025-02-20 23:41:55 -0300

    .

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 function exibirMensagemInicial() {
-    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('h1', 'Jogo do números secreto');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
 }
 
